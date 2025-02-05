a374b2aa69ec415a2a1dd541cd9b328a6ccb99be
Fabian Hanso <fabian.hanso@accenture.com>
2025-02-05 11:20:33 +0100

    change hover states for blog tiles and change navigations

--- components/Blog/BlogTile.tsx
+++ components/Blog/BlogTile.tsx
@@ -5,7 +5,7 @@ export default function BlogTile() {
     return (
         <a href='#' className='w-full h-auto flex flex-col group'>
             <div className='w-full aspect-video overflow-hidden bg-blue-500'>
-                <img src="./HeroImage.jpg" className='w-full h-full object-cover group-hover:opacity-40 transition-all'/>
+                <img src="./HeroImage.jpg" className='w-full h-full object-cover group-hover:opacity-40 group-hover:scale-105 transition-all'/>
             </div>
             <div className='mt-3 w-fit bg-blue-500 px-2 py-1 text-white text-xs group-hover:pl-3 transition-all flex gap-2'>
                <FireIcon className='hidden group-hover:block h-4 w-auto transition-all'/> Einsteiger-Guide
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -71,17 +71,14 @@ export default function Navbar() {
 
             <PopoverPanel
               transition
-              className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
+              className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
             >
               <div className="p-4">
                 {products.map((item) => (
                   <div
                     key={item.name}
-                    className="group relative flex items-center gap-x-6 rounded-md p-4 text-sm/6 hover:bg-gray-50"
+                    className="group relative flex items-center gap-x-6 p-4 text-sm/6 hover:bg-gray-50"
                   >
-                    <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                      <item.icon aria-hidden="true" className="size-6 text-gray-600 group-hover:text-indigo-600" />
-                    </div>
                     <div className="flex-auto">
                       <a href={item.href} className="block font-semibold text-gray-900">
                         {item.name}
@@ -90,18 +87,6 @@ export default function Navbar() {
                       <p className="mt-1 text-gray-600">{item.description}</p>
                     </div>
                   </div>
-                ))}
-              </div>
-              <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                {callsToAction.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-gray-900 hover:bg-gray-100"
-                  >
-                    <item.icon aria-hidden="true" className="size-5 flex-none text-gray-400" />
-                    {item.name}
-                  </a>
                 ))}
               </div>
             </PopoverPanel>
@@ -149,7 +134,7 @@ export default function Navbar() {
               <div className="space-y-2 py-6">
                 <Disclosure as="div" className="-mx-3">
                   <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                    Product
+                    Bereiche
                     <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-[open]:rotate-180" />
                   </DisclosureButton>
                   <DisclosurePanel className="mt-2 space-y-2">
@@ -169,27 +154,35 @@ export default function Navbar() {
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
-                  Features
+                  Blog
                 </a>
                 <a
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
-                  Marketplace
+                  Events
                 </a>
                 <a
                   href="#"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                 >
-                  Company
+                  Angebote
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                >
+                  Impressum
                 </a>
               </div>
               <div className="py-6">
                 <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  href="https://discord.gg/UuYMfATHzf"
+                  target='_blank'
+                  className="px-3 py-2.5 text-base/7 bg-blue-500 text-white hover:bg-stone-900 flex gap-2 justify-center items-center"
                 >
-                  Log in
+                  <img src='./Discord-Logos/discord-mark-white.svg' className='w-4 h-auto'/>
+                  Discord
                 </a>
               </div>
             </div>
