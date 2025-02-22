2b8b10833283908ce93ce20ab34427a24f217918
Fabian Hanso <fabian.hanso@accenture.com>
2025-02-22 01:24:24 +0100

    TypeScript errors

--- app/blog/[blogpost]/page.tsx
+++ app/blog/[blogpost]/page.tsx
@@ -1,13 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Post from "@/components/BlogPost/BlogPost";
 import client from "@/lib/contentful";
 
-type BlogPageProps = {
-    params: {
-      blogpost: string;
-    }
-  };
+// type BlogPageProps = {
+//     params: {
+//       blogpost: string;
+//     }
+//   };
 
-export default async function BlogPost(props: BlogPageProps) {
+export default async function BlogPost(props: any) {
 
   const { params } = props;
   const { blogpost } = params;
